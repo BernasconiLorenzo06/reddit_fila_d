d3f13d3a30bff30c65a8a02542a183720b729117
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-02-05 10:58:52 +0000

    punto 6

--- bernasconi/src/app/app.component.ts
+++ bernasconi/src/app/app.component.ts
@@ -8,9 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
  
-  autoTot(colore: HTMLInputElement, porte: HTMLInputElement, auto: HTMLInputElement): boolean {
-   
-    console.log (colore.value,porte.value,auto.value); // questo fa si che si possa visualizzare su CONSOLE
-    return false;
+  
+    vettore: any[];
+    constructor(){
+    this.vettore = []
+        }
+        ngOnInit(): void {
+        }
+    autoTot(colore: HTMLInputElement, porte: HTMLInputElement, auto: HTMLInputElement): boolean {
+     
+      console.log (colore.value,porte.value,auto.value); // questo fa si che si possa visualizzare su CONSOLE
+     
+      this.vettore.push(colore.value + "" + porte.value + "" + auto.value)
+      console.log(this.vettore)
+      return false;
 }
 }
